1a7efd8ebe7ec5b1b717d720fdea9631869d3fcc
Jalaj <67556827+jalaj-k@users.noreply.github.com>
2020-09-24 10:33:54 +0530

    Create AppBar

---
+++ frontend/src/styles/appbar.ts
@@ -0,0 +1,56 @@
+import { createStyles, Theme, fade } from "@material-ui/core/styles";
+
+export default (theme:Theme) => createStyles({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    fontFamily: "Comfortaa, cursive",
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    marginRight: theme.spacing(2),
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+      display: "block"
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "20ch",
+      "&:focus": {
+        width: "30ch",
+      },
+    },
+  },
+  icon: {
+    "& svg.MuiSvgIcon-root": {
+      fill: theme.palette.grey[200]
+    }
+  }
+});
