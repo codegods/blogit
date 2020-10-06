9d59b5cc6e76cd78c305a193d0ad3df3e3842a66
Jalaj <67556827+jalaj-k@users.noreply.github.com>
2020-10-06 18:30:07 +0530

    Create home page template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import {
   IconButton,
   Tooltip,
   LinearProgress,
-  Backdrop
+  Backdrop,
 } from "@material-ui/core";
 import {
   SearchOutlined as SearchIcon,
@@ -18,31 +18,33 @@ import {
   ExploreOutlined,
 } from "@material-ui/icons";
 import { Switch, Route } from "react-router-dom";
-import { appBarStyles, LoaderStyles } from "./styles/appbar";
+import Styles from "./styles/appbar";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-
+// Lazy load other app pages
 const Auth = React.lazy(() => import("./pages/auth/index"));
 
-let Loader = withStyles(LoaderStyles)(
-  class extends React.Component<WithStyles<typeof LoaderStyles>>{
-  render(){
-    const { classes } = this.props;
-    return (
-      <Backdrop open={true}>
-        <LinearProgress className={classes.progress} />
-      </Backdrop>
-    )
+// Fallback to show when any page is in loading state
+let Loader = withStyles(Styles.Loader)(
+  class extends React.Component<WithStyles<typeof Styles.Loader>> {
+    render() {
+      const { classes } = this.props;
+      return (
+        <Backdrop open={true}>
+          <LinearProgress className={classes.progress} />
+        </Backdrop>
+      );
+    }
   }
-})
+);
 
-class App extends React.Component<WithStyles<typeof appBarStyles>> {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
+// The app bar
+let MainAppBar = withStyles(Styles.AppBar)(
+  class extends React.Component<WithStyles<typeof Styles.AppBar>> {
+    render() {
+      const { classes } = this.props;
+      return (
+        <AppBar position="static" className={classes.root}>
           <Toolbar>
             <Typography className={classes.title} variant="h6" noWrap>
               blogit
@@ -79,15 +81,45 @@ class App extends React.Component<WithStyles<typeof appBarStyles>> {
             </Tooltip>
           </Toolbar>
         </AppBar>
-        <React.Suspense fallback={
-          <Loader />
-        }>
-            <Switch>
-              <Route path="/auth" component={Auth} />
-            </Switch>
+      );
+    }
+  }
+);
+
+// The main app
+class App extends React.Component<WithStyles<typeof Styles.App>> {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <MainAppBar />
+        <div className={classes.barProxy} />
+        <div className={classes.backdrop}>
+          <div className={classes.logo}>
+            <Typography variant="h1">
+              blogit
+            </Typography>
+            <Typography variant="h5">
+              The new era of blogging
+            </Typography>
+          </div>
+        </div>
+        <main className={classes.main}>
+          <Typography variant="h3">
+            A better place to write
+          </Typography>
+          <Typography variant="body1">
+            With blogit you can write whatever
+          </Typography>
+        </main>
+        <React.Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+          </Switch>
         </React.Suspense>
       </div>
     );
   }
 }
-export default withStyles(appBarStyles)(App);
+export default withStyles(Styles.App)(App);
