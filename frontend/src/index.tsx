cafcc759d12e8d719165d070ee122f08fa85f74b
Jalaj <axiom.jalaj.28@gmail.com>
2020-10-15 16:59:35 +0530

    Prevent double render on pre-rendered pages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,14 @@ import App from "./App";
 import theme from "./styles/theme";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+let render = ReactDOM.render;
+
+// If the app was pre-rendered, then we shouldn't re-render it
+// Instead, we should hydrate it.
+if (document.getElementById("app-root")?.innerHTML !== "")
+  render = ReactDOM.hydrate;
+
+render(
   <React.StrictMode>
     <Router>
       <ThemeProvider theme={theme}>
