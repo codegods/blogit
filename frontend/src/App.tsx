7f86ce95e385ab9767ca8a9777b6232b76dc001b
Jalaj <67556827+jalaj-k@users.noreply.github.com>
2020-11-30 15:58:58 +0530

    Create templates for User profile and about pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,8 @@ const HomePage = React.lazy(() => import("./components/Homepage"));
 const CreateNew = React.lazy(() => import("./pages/editor/index"));
 const Post = React.lazy(() => import("./pages/posts/index"));
 const Explore = React.lazy(() => import("./pages/explore/index"));
+const Profile = React.lazy(() => import("./pages/profile/index"));
+const AboutUs = React.lazy(() => import("./pages/meta/about"));
 
 // Fallback to show when any page is in loading state
 let Loader = withStyles(Styles.Loader)(
@@ -57,6 +59,8 @@ class App extends React.Component<WithStyles<typeof Styles.App>> {
             <Route path="/" exact component={HomePage} />
             <Route path="/auth/:page" component={Auth} />
             <Route path={url_for("views.posts")} component={Post} />
+            <Route path={url_for("views.user")} component={Profile} />
+            <Route path={url_for("views.meta.about")} component={AboutUs} />
             <PrivateRoute path={url_for("new")} component={CreateNew} />
             <PrivateRoute path={url_for("views.explore")} component={Explore} />
           </Switch>
