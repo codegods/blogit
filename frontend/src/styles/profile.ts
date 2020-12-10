df22bf3c931a9d601f05497b83df4602e127232e
Jalaj <67556827+jalaj-k@users.noreply.github.com>
2020-12-10 16:24:38 +0530

    Create profile page
    Although, it was alreday done. That was just a kind of template.
    Now it is fully functional except that following doesn't work as of now.

--- frontend/src/styles/profile.ts
+++ frontend/src/styles/profile.ts
@@ -2,29 +2,55 @@ import { createStyles, Theme } from "@material-ui/core/styles";
 
 export let RootStyles = (theme: Theme) => {
     return createStyles({
-        root: {
+        header: {
             display: "flex",
             "& > *": {
                 margin: theme.spacing(1.5),
             },
         },
-        dis: {
-            display: "normal",
-        },
         Avatar: {
-            width: theme.spacing(15),
-            height: theme.spacing(15),
+            height: "auto",
+            width: "auto",
+            borderRadius: "0%",
+            "& img": {
+                width: theme.spacing(15),
+                height: theme.spacing(15),
+                borderRadius: "50%",
+                [theme.breakpoints.down("xs")]: {
+                    width: theme.spacing(10),
+                    height: theme.spacing(10),
+                },
+            },
         },
         heading: {
             fontFamily: "'Comfortaa', cursive",
+            padding: theme.spacing(1),
+            "& div.MuiTypography-root": {
+                width: "100%",
+            },
         },
-        sub_heading: {
-            fontFamily: "'Comfortaa', cursive",
+        follow: {
+            "& div": {
+                display: "flex",
+                flexDirection: "row",
+            },
+            "& div .MuiTypography-root": {
+                fontSize: ".85rem",
+                fontWeight: "bold",
+                textAlign: "center",
+                margin: "0 8px",
+                flexGrow: 1
+            },
+            "& .MuiButton-root": {
+                width: "100%"
+            }
         },
-        paper: {
-            padding: theme.spacing(2),
+        content: {
+            margin: "8px 0"
+        },
+        info: {
             textAlign: "center",
-            color: theme.palette.text.secondary,
-        },
+            width: "100%"
+        }
     });
 };
